b963313f7b3e3f0bd4f57f4089a1cceff1927cad
mlim70 <kmmatthew8@gmail.com>
2025-09-13 18:34:35 -0400

    Updated executive committee members

--- frontend/src/pages/about/tabs/ExecutiveCommittee.tsx
+++ frontend/src/pages/about/tabs/ExecutiveCommittee.tsx
@@ -5,7 +5,6 @@ import React from 'react';
 import lalithaImage from '../../../assets/executive-committee/committee-lalitha.png';
 import satishImage from '../../../assets/executive-committee/committee-satish.jpg';
 import appavuchettyImage from '../../../assets/executive-committee/committee-appavuchetty.jpg';
-import jaiImage from '../../../assets/executive-committee/committee-jai.jpeg';
 import vishalImage from '../../../assets/executive-committee/committee-vishal.png';
 import raniImage from '../../../assets/executive-committee/committee-rani.jpg';
 
@@ -64,32 +63,22 @@ const ExecutiveCommittee: React.FC = () => {
 
               <div className="text-center">
                 <img 
+                  src={raniImage} 
+                  alt="Rani Reddy, MD"
+                  className="w-auto h-64 object-contain mx-auto mb-4 rounded-lg shadow-lg"
+                />
+                <h3 className="text-lg font-semibold text-neutral-dark mb-2">Rani Reddy, MD</h3>
+                <p className="text-red font-medium mb-1">Secretary 2025-26</p>
+              </div>
+
+              <div className="text-center">
+                <img 
                   src={lalithaImage} 
                   alt="Lalitha Medepalli, MD, FACC"
                   className="w-auto h-64 object-contain mx-auto mb-4 rounded-lg shadow-lg"
                 />
                 <h3 className="text-lg font-semibold text-neutral-dark mb-2">Lalitha Medepalli, MD, FACC</h3>
                 <p className="text-red font-medium mb-1">Past President 2024-25</p>
-              </div>
-
-              <div className="text-center">
-                <img 
-                  src={raniImage} 
-                  alt="Rani, MD"
-                  className="w-auto h-64 object-contain mx-auto mb-4 rounded-lg shadow-lg"
-                />
-                <h3 className="text-lg font-semibold text-neutral-dark mb-2">Rani, MD</h3>
-                <p className="text-red font-medium mb-1">Secretary 2025-26</p>
-              </div>
-
-              <div className="text-center">
-                <img 
-                  src={jaiImage} 
-                  alt="Jai Surana, MD"
-                  className="w-auto h-64 object-contain mx-auto mb-4 rounded-lg shadow-lg"
-                />
-                <h3 className="text-lg font-semibold text-neutral-dark mb-2">Jai Surana, MD</h3>
-                <p className="text-red font-medium mb-1">Treasurer 2025-26</p>
               </div>
 
              </div>
